aa7d9479db8e081e7fcce30a78d65b26928224f3
wayss000 <wayss000@gmail.com>
2018-03-29 00:39:33 +0800

    配置多个视图解析器

--- src/main/java/org/knight/controller/IndexConsole.java
+++ src/main/java/org/knight/controller/IndexConsole.java
@@ -1,6 +1,7 @@
 package org.knight.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author Wayss
@@ -8,4 +9,16 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class IndexConsole {
+
+    @RequestMapping(value = "/jsp")
+    public String jsp() {
+        System.out.println("jsp");
+        return "jsp";
+    }
+
+    @RequestMapping(value = "/html")
+    public String html() {
+        System.out.println("html");
+        return "html";
+    }
 }
---
+++ src/main/webapp/WEB-INF/jsp/jsp.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: SSS
+  Date: 2018/3/29
+  Time: 0:14
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+JSP Page.
+</body>
+</html>
